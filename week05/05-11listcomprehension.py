971cef4f59c489d526157885b01647fbc31d8ef7
TFQ_Moon <no1msh1217@gmail.com>
2021-04-25 02:49:10 +0900

    upload

--- week05/05-11listcomprehension.py
+++ week05/05-11listcomprehension.py
@@ -1,6 +1,6 @@
 # for문으로 리스트 생성
 a = []
-for i in range(10);
+for i in range(10):
     a.append(i)
 print(a)
 
@@ -9,13 +9,13 @@ seq = [i for i in range(10)]
 print(seq)
 
 # for 문으로 리스트 생성
-s= []
+s = []
 for i in range(10):
-    if i%2 == 1;
-        s.append(i**2)
+    if i % 2 == 1:
+        s.append(i ** 2)
 print(s)
 
 # 컴프리헨션으로 리스트 생성
-squares = [i**2 for i in range(10) if i%2 == 1]
+squares = [i ** 2 for i in range(10) if i % 2 == 1]
 
 # 컴프리헨션에선 콜론이 없다는 걸 주의 하자.
